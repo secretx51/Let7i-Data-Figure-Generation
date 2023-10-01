01f80d9f38cf29463ff97ecc0db82e13b2cf75c5
secretx51 <trent.neilson99@gmail.com>
2023-10-01 11:41:59 +1000

    Added gridlines, font Arial, black outlines

--- linkedOmicsPlot.py
+++ linkedOmicsPlot.py
@@ -1,18 +1,21 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import matplotlib as mpl
 from pathlib import Path
 
 MAIN_DIR = str(Path(__file__).resolve().parent) #Directory of python script
 COUNTS = f"{MAIN_DIR}/data/microRNA Counts.csv"
 OUTPUT = f"{MAIN_DIR}/data/microRNA formatted data.xlsx"
 
+# Set font family to Arial
+plt.rcParams['font.family'] = 'Arial'
+
 def countsBarGraph(filename, gene_number):
     df = pd.read_csv(filename)
     df_sorted = df.sort_values(by='Count', ascending=False)
     subset_df = df_sorted.iloc[:gene_number]
     subset_df['Gene'] = subset_df['Gene'].map(lowerCaseSignature)
+    
     # Set up the figure size for better visibility
     plt.figure(figsize=(8, 12))  # Width * Height Inches
     # Create the horizontal bar graph
@@ -22,14 +25,18 @@ def countsBarGraph(filename, gene_number):
     # Set the labels and title
     plt.xlabel('Number of Significant Immunological Related GO Pathways', fontsize = 14)
     #plt.title('Top 100 Genes by number immunological pathways according to LinkedOmics')
+    
     # Set the font size for y-axis labels
-    plt.tick_params(axis='y', labelsize=10)  # Adjust the font size as needed
-    # Add a grid for better readability (optional)
+    plt.tick_params(axis='y', labelsize=12)  # Adjust the font size as needed
+    plt.tick_params(axis='x', labelsize=12)  # Adjust the font size as needed
+    # Add a grid for better readability
     plt.grid(axis='x', linestyle='--', alpha=0.7)
+    
     # Remove dead space by adjusting y-axis limits
     factor = (100 / gene_number) * 2
     plt.ylim(top=subset_df['Count'].max() - 37,
             bottom=subset_df['Count'].max() + 58/factor)  # Add some padding at the top
+    
     # Save the figure, or use plt.show() to display it in the notebook
     plt.savefig(f'{MAIN_DIR}/plots/FigS1 - Histogram', dpi=600, bbox_inches='tight')
 
@@ -42,18 +49,24 @@ def outputHeatMap(filename, gene_number):
     
     trans_df = subset_df.transpose()
     trans_df["index"] = goDescriptionNames(trans_df.index)
+    trans_df["index"] = trans_df["index"].str.capitalize()
     trans_df.set_index("index", inplace=True)
-    trans_df = trans_df.loc[:"mast cell mediated immunity (GO:0002448)"]
+    trans_df = trans_df.iloc[:45] # Top 45 pathways
 
     plt.figure(figsize=(12, 9)) # Width * Height Inches
     # Generate the heatmap using seaborn's heatmap function
-    heatmap = sns.heatmap(trans_df, cmap='viridis', fmt='.1f', annot=False)
+    heatmap = sns.heatmap(trans_df, cmap='viridis', fmt='.1f', annot=False, 
+                        linewidths=0.5, linecolor='black')
+    
     # Get the color bar object from the heatmap
     cbar = heatmap.collections[0].colorbar
     # Set the font size of the color bar label
     cbar.ax.set_ylabel('-log10(p-value)', rotation=90, fontsize=14)
     # Set the fontsize of the color bar ticks
     cbar.ax.tick_params(labelsize=12)
+    # Set the color of the color bar border to black
+    cbar.outline.set_edgecolor('black')
+    cbar.outline.set_linewidth(0.5)
     # Move tick labels to left side of colour bar
     cbar.ax.yaxis.set_ticks_position('left')
     #cbar.ax.yaxis.set_label_position('left') #Move cbar label if needed
@@ -63,8 +76,10 @@ def outputHeatMap(filename, gene_number):
     # Force all x-axis tick labels to appear and adjust their font size
     plt.xticks(range(len(trans_df.columns)), trans_df.columns, fontsize=11.5, rotation=90)
 	# Set the title and labels for the plot
-    plt.ylabel('GO Immune Processes', fontsize=0)
-    plt.xlabel('Genes', fontsize=0)
+    # plt.ylabel('GO Immune Processes', fontsize=1)
+    # plt.xlabel('Genes', fontsize=1)
+    plt.ylabel('', fontsize=1) # remove axis labels
+    plt.xlabel('', fontsize=1)
 	# Save the figure, or use plt.show() to display it in the notebook
     plt.savefig(f'{MAIN_DIR}/plots/FigS2 - Heatmap', dpi=600, bbox_inches='tight')
 
@@ -161,57 +176,76 @@ def bubblePlot(filename, gene_number):
     # Set the figure size (in inches)
     plt.figure(figsize=(12, 18))  # Adjust width and height as needed
     plt.subplots_adjust(left=0.145, right=0.975)
+    ax = plt.gca()
+
     # Create a bubble plot for each group
     max_size = []
     for category, group_df in grouped_df:
         # Calculate bubble size and color for each row
         bubble_size = group_df.notnull().sum(axis=1)
-        max_size.append(bubble_size.max()) #Record all bubble sizes
+        max_size.append(bubble_size.max())  # Record all bubble sizes
         bubble_color = group_df.mean(axis=1, skipna=True)
         # Plot the bubble chart
-        ax = sns.scatterplot(x=[category] * len(group_df), y=group_df.index, 
-                        size=bubble_size, sizes=(10, 300), hue=bubble_color, alpha=0.7,
-                        palette="viridis", legend=False, cmap='viridis') # type: ignore
-    
-    # MAGIC - don't touch
+        """
+        Parameters:
+            x: The immune category to be plotted under as determined groupedCategories function
+            y: The gene to be plotted under as determined by groupedCategories function
+            size: Calculated number of pathways impacted for that gene in that category - sum
+            sizes: The range of bubble sizes, correlates with legend plot later
+            hue: The colour of each bubble calculated by the mean of the p-values 
+            alpha: Opacity of the bubbles, set to 1 to avoid seeing gridlines.
+            palette: The green-yellow colour gradient is called viridis
+            legend: False, as create custom legend later on
+            cmap: Same as hue.
+            ax: The plot variable to assign scatterplot to, was declared above. 
+            edgecolor: The black outline around the bubbles.
+            zorder: Put at higher zorder than gridlines so bubbles are above them.
+        """
+        sns.scatterplot(x=[category] * len(group_df), y=group_df.index,
+                        size=bubble_size, sizes=(10, 300), hue=bubble_color, alpha=1,
+                        palette="viridis", legend=False, cmap='viridis', ax=ax, 
+                        edgecolor='black', zorder = 2)
+
+    # Create the custom colour bar with correct p-value scale
     norm = plt.Normalize(bubble_color.min(), bubble_color.max())
     sm = plt.cm.ScalarMappable(cmap="viridis", norm=norm)
     sm.set_array([])
     # Add a colorbar
-    cbar = ax.figure.colorbar(sm, shrink = 0.5, 
-                            location = "right", anchor=(0.3,0.998)) #Set half size
-    # Label for colour bar
+    cbar = ax.figure.colorbar(sm, shrink=0.5,
+                            location="right", anchor=(0.3, 0.998))  # Set half size
+    # Label for color bar
     cbar.ax.set_ylabel('-log10(p-value)', rotation=90, fontsize=18)
     # Set the fontsize of the color bar ticks
     cbar.ax.tick_params(labelsize=16)
     cbar.ax.yaxis.set_ticks_position('left')
-    #cbar.ax.yaxis.set_label_position('left') #Move cbar label if needed
-        
+
     # Add legend for bubble sizes below the plot
-    bubble_sizes = range(1, max(max_size) + 1) #+1 to account for range ending 1 early
+    bubble_sizes = range(1, max(max_size) + 1)  # +1 to account for range ending 1 early
     legend_labels = [f'{size} non-missing values' for size in bubble_sizes]
     for size in bubble_sizes:
         plt.scatter([], [], s=size * 50, label=f'{size}', alpha=0.7, color='black')
-    # Set the title of bubble legned and size of bubble text
-    size_legend = plt.legend(scatterpoints=1, frameon=False, fontsize = 20,
-                            title='Number of\nImmune Related\nPathways', 
+    # Set the title of bubble legend and size of bubble text
+    size_legend = plt.legend(scatterpoints=1, frameon=False, fontsize=20,
+                            title='Number of\nImmune Related\nPathways',
                             loc='right', bbox_to_anchor=(1.295, 0.25))
     # Set size of the title
-    size_legend.set_title('Number of\nImmune Related\nPathways', 
+    size_legend.set_title('Number of\nImmune Related\nPathways',
                         prop={'size': 16})
     plt.gca().add_artist(size_legend)
     # Customize the plot
-    plt.ylabel('Genes', fontsize = 0)
+    # plt.ylabel('Genes', fontsize=20)
+    plt.ylabel('', fontsize=1) # Remove y axis label
     plt.xlabel('GO Categories', fontsize=20)
     plt.subplots_adjust(top=0.99, bottom=0.2)  # Adjust the top margin to reduce space above the title
-    # plt.title('Bubble Plot')
+
     # Rotate the x-axis labels by 45 degrees
-    plt.xticks(rotation=45, fontsize = 17, ha = 'right')
-    plt.yticks(fontsize = 17)
+    plt.xticks(rotation=45, fontsize=17, ha='right')
+    plt.yticks(fontsize=17)
+    
+    # Add grey grid lines
+    ax.grid(True, color='gray', linestyle='-', alpha=0.5, zorder = 2)
     # Save the plot at set dpi
     plt.savefig(f'{MAIN_DIR}/plots/Fig1 - Bubble Plot', dpi=600)
-    # Show the plot (if needed)
-    # plt.show()
 
 def main():
     countsBarGraph(COUNTS, 50)
